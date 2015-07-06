bc3e59b3cbb328dc54721d4ac7fefa4a5b049e06
Guilherme Baufaker Rêgo <gbre.111@gmail.com>
2015-07-06 17:20:30 -0300

    Seeds Modification

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,11 @@ user = User.new(:email => "gbre.111@gmail.com", name: "Guilherme Baufaker", encr
 
 user.save(validate: false)
 
-opinion = Opinion.new(:body => "Eu sou contra a redução da Maioridade Penal para 16 anos de idade", :user_id => user.id)
+user = User.new(:email => "cavalcanticunha.diego@gmail.com", name: "Diego Cunha", encrypted_password: "$2a$10$3OWXT/shvjpZ5tCU1auoSuqjWacETlUuMhX2R.A1HsqYjMnaqx3Oq", reset_password_token: nil, reset_password_sent_at: nil)
+
+user.save(validate: false)
+
+opinion = Opinion.new(:body => "Eu sou contra a redução da maioridade penal, porque não vai resolver o problema da violência. Temos a quarta maior população carcerária do mundo, com celas superlotadas, o Estado punindo os mais pobres e sem educação e serviço social suficiente", :user_id => user.id)
 discussionM.opinions << opinion
 
 
