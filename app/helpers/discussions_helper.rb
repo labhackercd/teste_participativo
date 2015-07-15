e20d6a51b68a4121def6cf87eab33add96a6579d
Guilherme Baufaker Rêgo <gbre.111@gmail.com>
2015-07-15 15:31:42 -0300

    First Refactor on Style

--- app/helpers/discussions_helper.rb
+++ app/helpers/discussions_helper.rb
@@ -18,4 +18,9 @@ module DiscussionsHelper
 
       end
   end
+
+  def relevancy_numbers(id, position)
+
+    return Opinion.where(parent_opinion_id: id, parent_position: position).count
+  end
 end
