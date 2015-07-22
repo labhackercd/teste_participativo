7c1e827ed1e0f8006de783b6c6498ab326789e93
Guilherme Baufaker Rêgo <gbre.111@gmail.com>
2015-07-22 14:45:42 -0300

    Refactoring: Move of Helper Methods to Correct Place

--- app/helpers/discussions_helper.rb
+++ app/helpers/discussions_helper.rb
@@ -1,7 +1,2 @@
 module DiscussionsHelper
-
-
-  def relevancy_numbers(id, position)
-    return Opinion.where(parent_opinion_id: id, parent_position: position).count
-  end
 end
---
+++ app/helpers/opinions_helper.rb
@@ -0,0 +1,14 @@
+module OpinionsHelper
+
+  def relevancy_numbers(id, position)
+    return Opinion.where(parent_opinion_id: id, parent_position: position).count
+  end
+
+  def users_who_interacted(id)
+    return Opinion.find(id).children.collect{|opinion| opinion.user}.uniq
+  end
+
+  def position_by_user(id, user)
+    return Opinion.find(id).children.collect{|opinion| opinion.parent_position if (opinion.user==user)}.compact
+  end
+end
