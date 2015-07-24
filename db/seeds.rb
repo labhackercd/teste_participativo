b6b654b47616180fd95e958ea3f0fa98ab56ccb9
Guilherme Baufaker Rêgo <gbre.111@gmail.com>
2015-07-24 15:45:28 -0300

    Opinions with Bars

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,15 @@ user = User.where(:name => /Diego/).first
 root_opinion = Opinion.create(:body => "teste", :user => user)
 
 
-another_opinion = Opinion.create(:body => "Teste23", :user => User.find_by(:name => /Guilherme/))
+another_opinion = Opinion.create(:body => "Teste23", :user => User.find_by(:name => /Guilherme/),  :parent_position => 'pro')
 
+another_opinion2 = Opinion.create(:body => "Teste234", :user => User.find_by(:name => /Diego/),  :parent_position => 'against')
 
 teste.opinions << root_opinion
 
 teste.opinions << another_opinion
+teste.opinions << another_opinion2
 
 root_opinion.children << another_opinion
+
+another_opinion.children << another_opinion2
